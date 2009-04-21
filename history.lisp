5cfafe96c2c7885e74a2869e679aea380159af59
David Sobeck <DSobeck@collabrx.com>
2009-04-21 13:11:31 -0700

    Comment out experimental code

--- history.lisp
+++ history.lisp
@@ -13,13 +13,13 @@
 
 
 
+#|
 (rdfs-def-class #$crx:bioblog/Entry ()
                 (#$crx:bioblog/in)
                 (#$crx:bioblog/out)
                 (#$crx:bioblog/user :range #$crx:bioblog/Entry))
 
 
-#|
 (rdfs-make-instance #$crx:bioblog/Entry
                     #$crx:bioblog/in in
                      #$crx:bioblog/out out
