31b46ecdab8030a8086c1541b4584d4cc8b3a0a5
Mike Travers <mt@collabrx.com>
2009-06-18 12:06:14 -0700

    actually add cl-statistcs, and cleanup
    --HG--
    branch : nlistener

---
+++ t/t-json.lisp
@@ -0,0 +1,30 @@
+(defun d-then-e (s)
+  (let* ((decoded (json->lisp s))
+	 (encoded     (mt:string-replace  (lisp->json decoded) "\"" "'")))
+    (print decoded)
+    (unless (equal s encoded)
+      (print `(differs ,s ,encoded)))))
+	
+
+from ~/.sbcl/site/cl-json_0.3.1/src/encoder.lisp   -- the real one seems broken
+;;; exp
+(defmethod encode-json((s list) stream)
+  (if (listp (car s))
+      (encode-json-alist s stream)
+      (call-next-method s stream)))
+
+This case loses
+(d-then-e "{'a': {'b': 'fred'}}")
+
+But this is OK..
+(d-then-e "{'a': {'b': 23}}")
+
+;;; +++ Should test out json extensions :empty-list and :empty-dict
+
+;;; Try to understand their weird encoding...
+(defun json->lisp (s)
+  (json:decode-json-from-string
+   (mt:string-replace 
+    (mt:string-replace s "'" "\"")
+    "None" "null")))
+
