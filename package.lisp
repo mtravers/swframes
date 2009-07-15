6e90aebdba6ff9c196a3751bc2d353cd290d1926
Mike Travers <mt@collabrx.com>
2009-07-14 17:23:46 -0700

    login and package fixes, mostly: can now use sw: and nl: funs from bioblog
    --HG--
    branch : nlistener

--- package.lisp
+++ package.lisp
@@ -1,5 +1,13 @@
 (in-package :cl-user)
 
+;;; Clean these up so we can use-package :swframes
+(unexport '(FRAMES:DESCRIBE-FRAME
+	    WEBLISTENER:PARSE-XML
+	    FRAMES:RENAME-FRAME
+	    FRAMES:DF
+	    FRAMES:SLOTV
+	    FRAMES:FOR-ALL-FRAMES))
+
 (defpackage :swframes
   (:use :cl :utils :clos*)
   (:nicknames :sw)
