e32e2668d8338d55bb23ed6e64e7b9ca638a76e8
Mike Travers <mt@collabrx.com>
2009-10-01 10:35:08 -0700

    adjust to unbroken domain system
    --HG--
    branch : nlistener

--- t/t-rdfs.lisp
+++ t/t-rdfs.lisp
@@ -3,5 +3,9 @@
 (define-test rdfs-basic
     (assert-error t (rdfs-make-instance #$crx:NotAClass))
   (rdfs-def-class #$crx:TestClass ())
-  (let ((i (rdfs-make-instance #$crx:TestClass)))
-    (write-frame i)))
+  (let ((i (rdfs-make-instance #$crx:TestClass #$crx:slot "foo")))
+    (write-frame i)
+    (assert-equal (list i)
+		  (rdfs-find "foo" :class #$crx:TestClass :slot  #$crx:slot))
+    (destroy-frame i)
+    ))
