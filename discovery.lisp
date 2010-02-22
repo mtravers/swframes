cd147118df906894fc62b93f93415120f4d8ca18
Mike Travers <mt@collabrx.com>
2010-02-22 10:37:53 -0800

    add missing file
    --HG--
    branch : clos

---
+++ discovery.lisp
@@ -0,0 +1,20 @@
+(in-package :sw)
+
+(export 'discover-classes)
+
+#|
+Still has some problems with the order of class creation.
+|#
+
+;;; Automatically generate CLOS classes from SPARQL endpoint
+(defmethod discover-classes ((endpoint sparql-endpoint))
+  #.(doc "Query the endpoint for RDFS classes that it contains, and define the corresponding CLOS classes")
+  (mapcar #'rdfs-clos-class
+	  (do-sparql-one-var endpoint
+	    `(:select (?concept) (:distinct t)
+		      (?x #$rdf:type ?concept)))))
+
+
+
+
+
